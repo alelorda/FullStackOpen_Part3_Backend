fffad51315b6ed4849a9ef2b5f49b6e6afaa70b3
Ariel Alexis Lorda Barrabasqui <ariel.lorda@inworx.com>
2023-10-02 20:16:37 -0300

    note added

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /*
  * importa el módulo de servidor web integrado de Node
  */
@@ -27,61 +26,18 @@
 // app.listen(PORT)
 // console.log(`Server running on port ${PORT}`)
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-   ]
-
-let  persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  },
-  {
-    id: 5,
-    name: "alexis",
-    number: "1234"
-  }
-]
-
+// lee las variables de entorno
+require('dotenv').config()
 // Importamos express
 const express = require('express')
-var morgan = require('morgan')
+const morgan = require('morgan')
+morgan('tiny')
+const Note = require('./models/note')
+const Person = require('./models/phonebook')
+const url = `mongodb+srv://fullstack:pio12667@cluster0.fldmyjv.mongodb.net/note-app?retryWrites=true&w=majority`
+
 const cors = require('cors')
 
-morgan('tiny')
 // Se crea una aplicación express almacenada en la variable app
 const app = express() 
 // Para acceder a los datos fácilmente se agrega json-parser para manejar las solicitudes HTTP POST:
@@ -90,18 +46,18 @@ app.use(express.static('build'))
 morgan.token("code", function getCode(req) {
   return JSON.stringify(req.body);
  });
- app.use(cors())
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :code'))
 //app.use(morgan(`:method :url :status :res[content-length] - :response-time ms {:response}`))
 // app.use(morgan.token('type', function (req, res) { return req.headers['content-type'] }))
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger)
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next()
+// }
+//app.use(requestLogger)
 /*
  * Definimos dos rutas a la aplicación. El primero define un controlador de eventos, que se utiliza para manejar las solicitudes HTTP GET 
  * realizadas a la raíz / de la aplicación: La función del controlador de eventos acepta dos parámetros. El params request contiene toda la información de la 
@@ -115,16 +71,23 @@ app.get('/', (request, response) => {                 // Primera ruta
   })
 
 // NOTES
-app.get('/api/notes', (request, response) => {        // Segunda ruta
+{
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
+// app.get('/api/notes/:id', (request, response) => {
+//   const note = notes.find(note => note.id === Number(request.params.id))
+//   if (note) response.json(note)
+//   // status establece el estado y end responde la solicitud sin enviar ningun dato.
+//   response.status(404).end()
+// })
 app.get('/api/notes/:id', (request, response) => {
-  const note = notes.find(note => note.id === Number(request.params.id))
-  if (note) response.json(note)
-  // status establece el estado y end responde la solicitud sin enviar ningun dato.
-  response.status(404).end()
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
-
 /*
  * notes.map(n => n.id) crea un nuevo array que contiene todos los ids de las notas. 
  * Math.max devuelve el valor máximo de los números que se le pasan. 
@@ -138,52 +101,83 @@ const generateId = () => {
     : 0
   return maxId + 1
 }
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response,next) => {
   const body = request.body
-  console.log(body)
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
 
-  const note = {
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  // Constructor
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
+  })
+
+  note.save().then(savedNote => savedNote.toJSON())
+  .then(savedAndFormattedNote => {
+    response.json(savedAndFormattedNote)
+  }).catch(error => next(error))
+})
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
-app.delete('/api/notes/:id', (request, response) => {
-  notes = notes.filter(note => note.id !== Number(request.params.id))
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
   /*
    * No hay consenso sobre qué código de estado debe devolverse a una solicitud DELETE, las únicas dos opciones son 204 y 404. 
    * Nuestra aplicación responderá con 204 en ambos casos.
    */
-  response.status(204).end()
-})
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+}
 
 // PERSONS
+{
 app.get('/api/persons',(request, response)=>{
-  response.json(persons)
+  Person.find({}).then(persons=>{
+    response.json(persons)
+  })
 })
-app.get('/api/persons/:id',(request, response)=>{
-  const res =persons.find(x=> x.id === Number(request.params.id))
-  if(res) response.json(res)
-  response.status(404).end()
+app.get('/api/persons/:id',(request, response, next)=>{
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      // console.log(error)
+      // response.status(400).send({ error: 'malformatted id' })}
+      next(error)
+    })
 })
-app.delete('/api/persons/:id',(request, response)=>{
-  persons = persons.filter(person => person.id !== Number(request.params.id))
+app.delete('/api/persons/:id',(request, response,next)=>{
+  Person.findByIdAndRemove(request.params.id)
+      .then(result => {
   /*
    * No hay consenso sobre qué código de estado debe devolverse a una solicitud DELETE, las únicas dos opciones son 204 y 404. 
    * Nuestra aplicación responderá con 204 en ambos casos.
    */
-  console.log(persons)
-  response.status(204).end()
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 app.post('/api/persons',(request,response)=>{
   const {name, number} = request.body;
@@ -194,40 +188,63 @@ app.post('/api/persons',(request,response)=>{
     })
   }
 
-if(validationName(name, number)){
-  return response.status(400).json({ 
-    error: 'Name must be unique' 
+  // if(validationName(name, number)){
+  //   return response.status(400).json({ 
+  //     error: 'Name must be unique' 
+  //   })
+  // }
+
+  // Constructor
+  const newPerson = new Person({
+    name: name,
+    number: number,
   })
+  newPerson.save().then(savedPerson=>{
+    response.json(savedPerson)
+  })
+})
 }
 
-  const newPerson = {
-    name: request.body.name,
-    number: request.body.number,
-    id: Math.floor(Math.random() * 100)
-  }
-  persons.push(newPerson)
-  response.json(persons)
-})
-
 // INFO
+{
 app.get('/api/info',(request, response)=>{
   response.send(`<p>Phonebook has info for ${persons.length} people</p><br/><p>${new Date()}</p>`)
 })
+}
 
+/*
+ * El orden de ejecución del middleware es el mismo que el orden en el que se cargan en express con la función app.use. 
+ * Por esta razón, es importante tener cuidado al definir el middleware.
+ * El orden correcto es el siguiente: 1° unknownEndpoint - 2° errorHandler
+ */
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+// Los manejadores de errores de Express son middleware que se definen:
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
 
 function validationName(name, number) {
-  return persons.find(
-    (person) =>
-      person.name === name || person.number === number
-  );
+  Person.find({}).then(persons=>{
+    response.json(persons)
+  })
 };
---
+++ mongo.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+const url = `mongodb+srv://fullstack:${password}@cluster0.fldmyjv.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+const Note = mongoose.model('Note', noteSchema)
+const note = new Note({
+    content: 'HTML is Easy',
+    date: new Date(),
+    important: true,
+  })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })  
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+const Person = mongoose.model('Person', personSchema)
+if(name && number){
+    
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+    person.save().then(result =>{
+        console.log('Person saved!')
+        mongoose.connection.close()
+    })
+}
+else{
+Person.find({}).then(result => {
+    console.log('phonebook')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+      console.log(`${person.id}`);
+    })
+    mongoose.connection.close()
+  })
+}
+
+
+// Note.find({}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
+
+//   Note.find({important:true}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
+
+  
